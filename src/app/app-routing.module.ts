5557dc6c46b28347ae970ddf5a28b9f40ac960bf
sillyhat <xushikuan1@163.com>
2019-08-09 23:09:56 +0800

    install project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {/* 默认路由 */
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {/* 匹配不到路由时跳转的页面 404 */
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
